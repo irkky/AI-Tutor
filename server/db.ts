bf47ca413510910f003c11323f15facdeee400cf
Rishabh Kumar Kannaujiya <rishabhkrkannaujiya@gmail.com>
2025-11-07 19:38:18 +0530

    neon converted to supabase

--- server/db.ts
+++ server/db.ts
@@ -1,12 +1,8 @@
-import "dotenv/config";
-import { drizzle } from "drizzle-orm/neon-http";
-import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/node-postgres";
+import pg from "pg";
 import * as schema from "../shared/schema.js";
 
-const sql = neon(process.env.POSTGRES_URL!);
-
-export const db = drizzle(sql, { 
-  schema: {
-    ...schema,
-  }
+const pool = new pg.Pool({
+  connectionString: process.env.POSTGRES_URL,
 });
+export const db = drizzle(pool, { schema });
