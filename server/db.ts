8935b1ba19f21e1a3d70becd6317c59a4e72c606
Rishabh Kumar Kannaujiya <rishabhkrkannaujiya@gmail.com>
2025-11-07 19:49:26 +0530

    made changes to sslmode=require

--- server/db.ts
+++ server/db.ts
@@ -3,9 +3,6 @@ import pg from "pg";
 import * as schema from "../shared/schema.js";
 
 const pool = new pg.Pool({
-  connectionString: process.env.POSTGRES_URL,
-  ssl: {
-    rejectUnauthorized: false, // This bypasses the certificate chain validation
-  },
+  connectionString: `${process.env.POSTGRES_URL}?sslmode=require`,
 });
 export const db = drizzle(pool, { schema });
